6aff3018ded0e1980e98628cbbcf872d444f495f
tiffanywalker22 <tbcasper16@gmail.com>
2023-07-07 07:46:13 -0700

    add variables

--- _printf.c
+++ _printf.c
@@ -10,6 +10,8 @@ int _printf(const char *frmt, ...)
 {
 	if (format != NULL) /*checks if format is null*/
 
-	{
+	{	int count = 0, i; /*count keeps track of the number of chars being printed*/
+		int (*m) (va_list);
+		va_list args; /*args as variable type for va_list typefor va_list to access variable arguments*/
 		
 }
