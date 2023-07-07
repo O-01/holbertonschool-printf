4db7c08c43b0787fccacd7fb6a8864a1dfd0d1f3
tiffanywalker22 <tbcasper16@gmail.com>
2023-07-07 08:57:04 -0700

    add format specifier

--- _printf.c
+++ _printf.c
@@ -13,18 +13,13 @@ int _printf(const char *frmt, ...)
 	{	int count = 0, i; /*count keeps track of the number of
 				  chars being printed*/
 		int (*m) (va_list);
-		va_list args; /*args as variable type for va_list type
-				for va_list to access variable args*/
+		va_list args; /*args as variable type for va_list type for va_list to access variable args*/
 
-		va_start (args, format); /*va_start macro initalize
-					   args*/
+		va_start (args, format); /*va_start macro initalize args*/
 		i = 0;
-		while (format[i]) /*while loop iterates over
-					    chars in string until \0*/
+		while (format[i]) /*while loop iterates over chars in string until \0*/
 		{
-			if (format[i] == '%') /*checks if current char
-						is % which indicates
-						start of format spec*/
+			if (format[i] == '%') /*checks if current char is % which indicates start of format spec*/
 		}
 			if (format[i + 1] == '%') /*if next char after
 						  % is also %, means
@@ -36,4 +31,14 @@ int _printf(const char *frmt, ...)
 			}
 			else
 			}
+				m = get_func(format[i + 1]);
+				if (m)
+					count += m(args);
+				else
+					count += _putchar(format[i]) + _putchar(format[i + 1]);
+				i += 2;
+			}
+					
+
+
 }
