92c677c7999bdc21af3452d98eed803a97f9d73e
O-01 <sa.an-s@yandex.com>
2023-07-07 10:28:11 -0700

    Edits to make program compile

--- _printf.c
+++ _printf.c
@@ -10,15 +10,14 @@
 
 int _printf(const char *frmt, ...)
 {
+	va_list args; /*args as variable type for va_list type for va_list to access variable args*/
+	int count = 0, i; /*count keeps track of the number of chars being printed*/
+/*	int (*m)(va_list);*/
+
+	va_start (args, frmt); /*va_start macro initalize args*/
+
 	if (frmt) /*checks if format is null*/
-
-	{	
-		int count = 0, i; /*count keeps track of the number of
-				  chars being printed*/
-		int (*m) (va_list);
-		va_list args; /*args as variable type for va_list type for va_list to access variable args*/
-
-		va_start (args, frmt); /*va_start macro initalize args*/
+	{
 		i = 0;
 		while (frmt[i]) /*while loop iterates over chars in string until \0*/
 		{
@@ -28,20 +27,17 @@ int _printf(const char *frmt, ...)
 				{
 					count += _putchar(frmt[i]);
 			       		i += 2;
-				}
+				}/*
 				else
 				{
-
 					m = get_func(frmt[i + 1]);
-					if (m) 
-					{
+					if (m)
 						count += m(args);
-					}
 					else
-						count += _putchar(frmt[i]) + _putchar(frmt[i + 1])LL:;
+						count += _putchar(frmt[i]) + _putchar(frmt[i + 1]);
 					i += 2;
-				}	
-			{
+					}*/
+			}
 			else
 			{
 				count += _putchar(frmt[i]);
@@ -51,5 +47,4 @@ int _printf(const char *frmt, ...)
 	}
 	va_end(args);
 	return (count);
-	}
 }
