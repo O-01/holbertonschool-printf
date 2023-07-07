1d52a7c3ce35cf226d6ecd2fef03032c6db5381e
tiffanywalker22 <tbcasper16@gmail.com>
2023-07-07 09:31:59 -0700

    working on errors

--- _printf.c
+++ _printf.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdarg.h>
+#include <stddef.h>
 
 /**
  *
@@ -8,7 +10,7 @@
 
 int _printf(const char *frmt, ...)
 {
-	if (format != NULL) /*checks if format is null*/
+	if (frmt != NULL) /*checks if format is null*/
 
 	{	
 		int count = 0, i; /*count keeps track of the number of
@@ -16,40 +18,40 @@ int _printf(const char *frmt, ...)
 		int (*m) (va_list);
 		va_list args; /*args as variable type for va_list type for va_list to access variable args*/
 
-		va_start (args, format); /*va_start macro initalize args*/
+		va_start (args, frmt); /*va_start macro initalize args*/
 		i = 0;
-		while (format[i]) /*while loop iterates over chars in string until \0*/
+		while (frmt[i]) /*while loop iterates over chars in string until \0*/
 		{
-			if (format[i] == '%') /*checks if current char is % which indicates start of format spec*/
-		}
-			if (format[i + 1] == '%') /*if next char after
-						  % is also %, means
-						string contains a
-					      literal %*/	
+			if (frmt[i] == '%') /*checks if current char is % which indicates start of format spec*/
 			{
-				count += _putchar(format[i]);
+			if (frmt[i + 1] == '%') /*if next char after % is also %, means string contains a literal %*/	
+			{
+				count += _putchar(frmt[i]);
 			       	i += 2;
 			}
 			else
 			}
-				m = get_func(format[i + 1]);
-				if (m)
+				m = get_func(frmt[i + 1]);
+				if (m) 
+				{
 					count += m(args);
+				}
 				else
-					count += _putchar(format[i]) + _putchar(format[i + 1]);
+				{
+					count += _putchar(frmt[i]) + _putchar(frmt[i + 1]);
+				}
 				i += 2;
-			}
+			}	
 		}
 		else
 		{
-			count += _putchar(format[i]);
+			count += _putchar(frmt[i]);
 			i++;
 		}
-	{
+	}
 	va_end(args);
 	return (count);
 	}
 
-return (-1);
-
+	return (-1);
 }
