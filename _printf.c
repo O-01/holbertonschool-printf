95ab4afb04788a88d0532e75302446e552ff6df8
tiffanywalker22 <tbcasper16@gmail.com>
2023-07-07 07:56:52 -0700

    add initilization for args

--- _printf.c
+++ _printf.c
@@ -10,8 +10,15 @@ int _printf(const char *frmt, ...)
 {
 	if (format != NULL) /*checks if format is null*/
 
-	{	int count = 0, i; /*count keeps track of the number of chars being printed*/
+	{	int count = 0, i; /*count keeps track of the number of
+				  chars being printed*/
 		int (*m) (va_list);
-		va_list args; /*args as variable type for va_list typefor va_list to access variable arguments*/
+		va_list args; /*args as variable type for va_list type
+				for va_list to access variable args*/
+
+		va_start (args, format); /*va_start macro initalize
+					   args*/
+		i = 0;
+		while (format[i] != '\0')
 		
 }
