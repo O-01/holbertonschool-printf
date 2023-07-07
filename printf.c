002f00f8ac39fd20c85f0169cb7a07f904b3c5a6
O-01 <sa.an-s@yandex.com>
2023-07-07 13:37:35 -0700

    Minor rework

---
+++ printf.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stddef.h>
+
+/**
+ * _printf - replica of printf
+ *@frmt: string to print
+ *Return: the chars printed from string
+ */
+
+int _printf(const char *frmt, ...)
+{
+	va_list args;
+	int i, x = 0;
+	char *yank = 0;
+
+	va_start(args, frmt);
+	while (frmt && frmt[i])
+	{
+		*yank = frmt[i];
+		if (*yank == '%')
+		{
+			i++;
+			if (get_func(yank)(args))
+				get_func(yank)(args);
+		}
+		else
+			_putchar(frmt[i]);
+		i++;
+	}
+
+	va_end(args);
+	return (x);
+}
