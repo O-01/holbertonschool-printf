60c1c5e33b8d0d85d631591a8e9a2dad034ff5b1
O-01 <sa.an-s@yandex.com>
2023-07-07 10:43:28 -0700

    Added get_func

--- get_func.c
+++ get_func.c
@@ -1,8 +1,28 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
 
 /**
- *
- *
- *
+ * get_func - determines function to return based on input x
+ * @x: input compared against entries in struct det
+ * Return: function corresponding to input x
  */
 
+int (*get_func(char *x))(va_list)
+{
+	wv det[] = {
+		{"c", c_printer},
+		{"s", s_printer},
+		{"d", d_printer},
+		{"i", i_printer},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	if (x)
+		while (det[i].spec && *(det[i].spec) != *x)
+			i++;
+
+	return (det[i].func);
+}
